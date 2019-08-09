3b85206917cbaf42b98827b73187858e868e874e
Eugene <e.kuzmenko@dunice.net>
2019-08-09 14:50:42 +0300

    #task 4 ready. Allow rendering of nested lists

--- converters/tests/to_html.py
+++ converters/tests/to_html.py
@@ -44,6 +44,55 @@ class TestToHTML(unittest.TestCase):
             ])
         )
 
+    def test_nested(self):
+        self.assertEqual(
+            ''.join([
+                '<ul>',
+                    '<li>',
+                        '<h1>title 1</h1>',
+                        '<p>body 1</p>',
+                    '</li>',
+                    '<li>'
+                        '<h1>title 2</h1>',
+                        '<p>body 2</p>',
+                    '</li>',
+                    '<li>',
+                        '<section>'
+                            '<ul>',
+                                '<li>',
+                                    '<h3>Level 2</h3>',
+                                '</li>',
+                                '<li>',
+                                    '<h3>Status:</h3>',
+                                    '<span>Achieved</span>',
+                                '</li>',
+                            '</ul>',
+                        '</section>',
+                        '<div>here</div>',
+                    '</li>',
+                '</ul>'
+            ]),
+            to_html([
+                OrderedDict([
+                    ('h1', 'title 1'), ('p', 'body 1'),
+                ]),
+                OrderedDict([
+                    ('h1', 'title 2'), ('p', 'body 2'),
+                ]),
+                OrderedDict([
+                    ('section', [
+                        OrderedDict([
+                            ('h3', 'Level 2'),
+                        ]),
+                        OrderedDict([
+                            ('h3', 'Status:'), ('span', 'Achieved')
+                        ]),
+                    ]),
+                    ('div', 'here')
+                ])
+            ])
+        )
+
     def test_not_list(self):
         with self.assertRaises(TypeError) as context:
             to_html({
@@ -54,3 +103,22 @@ class TestToHTML(unittest.TestCase):
             self.assertTrue(
                 'Expected type <class \'list\'>. Got <class \'dict\'>' in context.exeptions
             )
+
+    def test_children_not_dict(self):
+        with self.assertRaises(TypeError) as context:
+            to_html([
+                OrderedDict([
+                    ('h1', 'title 1'), ('p', 'body 1'),
+                ]),
+                OrderedDict([
+                    ('h1', 'title 2'), ('p', 'body 2'),
+                ]),
+                OrderedDict([
+                    ('section', OrderedDict([
+                        ('h3', 'Level 2'),
+                    ])),
+                    ('div', 'here')
+                ])
+            ])
+
+            self.assertTrue('Tag content can\'t be instance of Dict')
